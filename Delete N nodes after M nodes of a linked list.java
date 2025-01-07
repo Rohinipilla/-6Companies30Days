1a2db68bee05fc8b1dfc27af47447b0449ab57d8
PILLA ROHINI <138279046+Rohinipilla@users.noreply.github.com>
2025-01-07 19:52:36 +0530

    Create Delete N nodes after M nodes of a linked list.java

---
+++ Delete N nodes after M nodes of a linked list.java
@@ -0,0 +1,30 @@
+class Solution {
+    static void linkdelete(Node head, int n, int m) {
+        // your code here
+        Node curr = head; 
+        Node t;
+        int count; 
+
+        while (curr != null) {
+
+            for (count = 1; count < m && curr != null; count++){
+                curr = curr.next;
+            }
+            
+            if (curr == null){
+                return;
+            }
+                
+            t = curr.next;
+            for (count = 1; count <= n && t != null; count++) {
+                Node temp = t;
+                t = t.next;
+                temp = null;
+            }
+            curr.next = t;
+            curr = t;
+        }
+
+        
+    }
+}
