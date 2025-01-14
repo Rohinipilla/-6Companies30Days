bbc11b0a5501adb8a7f4ad9f046d3fd844f3844a
PILLA ROHINI <138279046+Rohinipilla@users.noreply.github.com>
2025-01-14 20:32:37 +0530

    Create 216. Combination Sum III.java

---
+++ 216. Combination Sum III.java
@@ -0,0 +1,29 @@
+class Solution {
+     private void helper(int k, int n, int value, List<List<Integer>> combinations,
+            List<Integer> combination) {
+        int combinationSize = combination.size();
+
+        if (combinationSize == k && n == 0) {
+            combinations.add(new ArrayList<>(combination));
+            
+            return;
+        }
+
+        for (int i = value; i < 10; i++) {
+            if (combinationSize > k || n < 0)
+                break;
+
+            combination.add(i);
+
+            helper(k, n - i, i + 1, combinations, combination);
+
+            combination.remove(combination.size() - 1);
+        }
+    }
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> combinations = new ArrayList<>();
+        helper(k,n,1,combinations,new ArrayList<>());
+
+        return combinations;
+    }
+}
