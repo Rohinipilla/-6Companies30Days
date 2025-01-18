6c2c514a84044bd88dbfa6b8f8e7b86a09742122
PILLA ROHINI <138279046+Rohinipilla@users.noreply.github.com>
2025-01-18 19:41:54 +0530

    Create 23. Merge k Sorted Lists.java

---
+++ 23. Merge k Sorted Lists.java
@@ -0,0 +1,22 @@
+class Solution {
+    private ListNode helper(ListNode l1,ListNode l2){
+        if(l1==null)return l2;
+        if(l2==null)return l1;
+        if(l1.val<l2.val){
+          l1.next= helper(l1.next,l2);
+          return l1;
+        }
+        else{
+          l2.next= helper(l2.next,l1);
+          return l2;
+        }
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0) return null;
+        ListNode ans=null;
+        for(int i=0;i<lists.length;i++){
+            ans=helper(lists[i],ans);
+        }
+        return ans;
+    }
+}
